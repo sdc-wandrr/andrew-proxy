dd8709f21942df84f200a68ef3061cc03ea2a037
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-09-06 20:14:36 -0700

    installed artillery and newrelic

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+require('newrelic');
 const app = express();
 const PORT = 3030;
 
