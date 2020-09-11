6aa1e25d86ecbe0512f55f855f26b35bf7b0b958
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-09-11 15:38:38 -0700

    reworked proxy  routes to load the external services

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('newrelic');
+
+const services = require('./services_config.js');
 const app = express();
 const PORT = 3030;
 
@@ -10,26 +12,31 @@ app.use((req, res, next) => {
   console.log(`serving ${req.method} request for ${req.url}`);
   next();
 });
+
+// Initial index.html
 app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/hostels/:hostel_id', express.static(path.join(__dirname, '../public')));
-app.use('/dist/bundle.js', createProxyMiddleware({ target: 'http://172.31.32.92:3001/', changeOrigin: true }));
 
-// API reroutes
+// ReRoutes
 
-// Image carousel service
-// app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: 'http://localhost:3007', changeOrigin: true }));
+// Image Carousel Service
+app.use('images/', createProxyMiddleware({ target: carouselServiceURL, changeOrigin: true }));
+app.use('/api/hostels/:hostel_id/images', createProxyMiddleware({ target: carouselServiceURL, changeOrigin: true }));
+
+// Info Service
+app.use('info/', createProxyMiddleware({ target: descriptionServiceURL, changeOrigin: true }));
+app.use('/house/:id/hostel', createProxyMiddleware({ target: descriptionServiceURL, changeOrigin: true }));
+app.use('/house/:id/description', createProxyMiddleware({ target: descriptionServiceURL, changeOrigin: true }));
+app.use('/house/:id/rules', createProxyMiddleware({ target: descriptionServiceURL, changeOrigin: true }));
+app.use('/house/:id/address', createProxyMiddleware({ target: descriptionServiceURL, changeOrigin: true }));
+
+// Reviews Service
+app.use('/reviews/bundle.js', createProxyMiddleware({ target: reviewServiceURL, changeOrigin: true }));
+app.use('/hostels/:id/api/reviews', createProxyMiddleware({ target: reviewServiceURL, changeOrigin: true }));
+app.use('/api/reviews', createProxyMiddleware({ target: reviewServiceURL, changeOrigin: true }));
+app.use('/api/reviews/:id', createProxyMiddleware({ target: reviewServiceURL, changeOrigin: true }));
+
 // availability service
 // app.use('/api/hostel/:hostelId/rooms', createProxyMiddleware({ target: 'http://localhost:3009', changeOrigin: true }));
-// description map rules service
-// app.use('/house/:id/hostel', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
-// app.use('/house/:id/description', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
-// app.use('/house/:id/rules', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
-// app.use('/house/:id/address', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
-// reviews service
-app.use('/hostels/:id/api/reviews', createProxyMiddleware({ target: 'http://172.31.32.92:3001/', changeOrigin: true }));
-app.use('/api/reviews', createProxyMiddleware({ target: 'http://172.31.32.92:3001/', changeOrigin: true }));
-app.use('/api/reviews/:id', createProxyMiddleware({ target: 'http://172.31.32.92:3001/', changeOrigin: true }));
-
-
 
 app.listen(PORT, () => console.log('Proxy Server listening on port ', PORT));
