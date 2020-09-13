2500cb2b9b795bd5509566434f3e54ea7f9fbecc
andrewdeanyoder <andrew.dean.yoder@gmail.com>
2020-09-13 13:56:08 -0700

    updated IP to use load balancer

--- server/services_config.js
+++ server/services_config.js
@@ -1,5 +1,5 @@
 module.exports = {
   carouselServiceURL: 'http://3.101.103.127',
   descriptionServiceURL: 'http://54.215.89.194',
-  reviewServiceURL:'http://34.239.107.185/',
+  reviewServiceURL:'http://172.31.61.157',
 };
